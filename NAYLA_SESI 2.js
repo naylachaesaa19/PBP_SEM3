442f07fe89b0584c0f870f4a81072e88ea521601
naylachaesaa19 <nayla.chaesa_ti24@nusaputra.ac.id>
2025-09-24 16:42:01 +0700

    Add files via upload

---
+++ NAYLA_SESI 2.js
@@ -0,0 +1,21 @@
+let input = prompt("Masukkan nilai (0-100): ");
+let nilai = parseInt(input);
+let huruf;
+
+if (isNaN(nilai) || nilai < 0 || nilai > 100) {
+  alert("Input tidak valid! Tolong masukkan angka antara 0-100.");
+} else if (nilai >= 85) {
+  huruf = "A";
+} else if (nilai >= 70) {
+  huruf = "B";
+} else if (nilai >= 55) {
+  huruf = "C";
+} else if (nilai >= 40) {
+  huruf = "D";
+} else {
+  huruf = "E";
+}
+
+if (huruf) {
+alert(`Nilai kamu: ${nilai} \nNilai huruf: ${huruf}`);
+}
